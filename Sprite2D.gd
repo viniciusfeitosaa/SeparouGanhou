4b42dd31abaa1d3809652c1b03af488241fa3f25
Vinícius Feitosa <viniciusfeitosa@protonmail.com>
2024-11-04 21:41:31 -0300

    Initial commit

---
+++ Sprite2D.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+var speed = 100
+
+func _process(delta):
+	position.y += speed * delta
+	if position.y > get_viewport().size.y:
+		queue_free() # Remove o lixo quando sai da tela
+
